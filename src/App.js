a0046b5cb64babc06b591302921f4ef5723c3c22
Dechathorn <59277863+dechathorn2148@users.noreply.github.com>
2021-02-02 13:52:56 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React, { useState } from "react";
+import AppTable from "./component/AppTable";
+
 function App() {
   const persons = [
     {
@@ -9,34 +12,33 @@ function App() {
       name: "Alice",
       gender: "male",
       age: "20"
+    },
+    {
+      name: "Dechathorn Intravijit 620612148",
+      gender: "male",
+      age: "19"
     }
   ];
+
+  const personsElements = persons.map((person, index) => {
+    return <AppTable key={index} person={person} />;
+  });
+
+  const [count, setCount] = useState(620612148);
+
   return (
     <div class="ml-2">
       <h3 class="title is-3">Person List</h3>
 
       {/* Convert me to a component! */}
-      <table class="table is-bordered mb-3">
-        <tbody>
-          <tr>
-            <th>Name</th>
-            <th>Gender</th>
-            <th>Age</th>
-          </tr>
-          <tr>
-            <td>Bob</td>
-            <td>male</td>
-            <td>50</td>
-          </tr>
-        </tbody>
-      </table>
+      {personsElements}
 
       {/* Code me please! */}
       <h3 class="title is-3">ID Counter</h3>
-      <p>YOUR ID HERE</p>
-      <button>-</button>
-      <button>reset</button>
-      <button>+</button>
+      <p>{count}</p>
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <button onClick={() => setCount(620612148)}>reset</button>
+      <button onClick={() => setCount(count + 1)}>+</button>
     </div>
   );
 }
